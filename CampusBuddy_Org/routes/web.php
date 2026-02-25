d053b5b65d6c49aa78da0080f7b66e5d3b106406
washimakram013099@gmail.com <washimakram013099@gmail.com>
2026-02-25 11:28:34 +0600

    Updated Forget password section by Washim

--- CampusBuddy_Org/routes/web.php
+++ CampusBuddy_Org/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ForgotPasswordController;
 
 Route::get('/', function () {
     return redirect('/login');
@@ -12,6 +13,14 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login.post');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Forgot Password
+Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('forgot-password');
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink'])->name('forgot-password.send');
+
+// Reset Password
+Route::get('/reset-password', [ForgotPasswordController::class, 'showResetForm'])->name('reset-password');
+Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword'])->name('reset-password.update');
 
 // Dashboard
 Route::get('/Dashboard1', function () {
